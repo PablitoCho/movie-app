bbe5c0b3eb22c26c2b23a25b6a3195a1236b6509
woncho <woncho@gmarket.com>
2023-01-08 22:39:23 +0900

    16. MovieList 컴포넌트

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,9 +8,18 @@ const store = new Store({
 
 export default store
 
-// my key : 812fae45
+// test key : 7035c60c
+// ex : https://www.omdbapi.com/?s=frozen&apikey=812fae45
 export const searchMovies = async page => {
-  const res = await fetch(`https://www.omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
-  const json = await res.json()
-  console.log(json)
+  if(page==1) {
+    store.state.page = 1
+    // 새로운 검색시 page가 1로 들어온다
+    store.state.movies = []
+  }
+  const res = await fetch(`https://www.omdbapi.com?apikey=812fae45&s=${store.state.searchText}&page=${page}`)
+  const { Search } = await res.json()
+  store.state.movies = [
+    ...store.state.movies, // 이전에 가지고 온 영화 정보
+    ...Search // 새로 가지고 온 영화 정보
+  ]
 }
