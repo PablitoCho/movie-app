2185e87c0247274aae760025258cdd3188716c16
woncho <woncho@gmarket.com>
2023-01-14 10:39:17 +0900

    20. 예외 처리와 메시지 출력

--- src/components/MovieListMore.js
+++ src/components/MovieListMore.js
@@ -15,9 +15,10 @@ export default class MovieListMore extends Component {
   }
   render() {
     this.el.classList.add('btn', 'view-more', 'hide')
-    this.el.textContent = 'View mote..'
+    this.el.textContent = 'View more..'
 
     this.el.addEventListener('click', async () => {
+      this.el.classList.add('hide')
       await searchMovies(movieStore.state.page + 1) // 다음 페이지 요청
     })
   }
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,8 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
-  loading : false
+  loading : false,
+  message : 'Search for the movie title!'
 })
 
 export default store
@@ -17,13 +18,25 @@ export const searchMovies = async page => {
   store.state.page = page
   if(page==1) { // 새로운 검색시 page가 1로 들어온다
     store.state.movies = []
+    store.state.message = ''
   }
-  const res = await fetch(`https://www.omdbapi.com?apikey=812fae45&s=${store.state.searchText}&page=${page}`)
-  const { Search, totalResults } = await res.json()
-  store.state.movies = [
-    ...store.state.movies, // 이전에 가지고 온 영화 정보
-    ...Search // 새로 가지고 온 영화 정보
-  ]
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-  store.state.loading = false
+  try {
+    const res = await fetch(`https://www.omdbapi.com?apikey=812fae45&s=${store.state.searchText}&page=${page}`)
+    const { Search, totalResults, Response, Error } = await res.json()
+    if(Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies, // 이전에 가지고 온 영화 정보
+        ...Search // 새로 가지고 온 영화 정보
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      // Reponse === 'False'
+      store.state.message = Error
+    }
+  } catch(error) {
+    // fetch 함수는 다양한 이유로 에러가 발생할 수 있다.
+    console.log('searchMovies error:', error)
+  } finally {
+    store.state.loading = false
+  }  
 }
