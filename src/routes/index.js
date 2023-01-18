04a380c070b1b352dc84f166111830ce40a1a6c0
woncho <woncho@gmarket.com>
2023-01-18 16:03:03 +0900

    29. 404 Page Not Found

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { createRouter } from '../core/heropy'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 
 export default createRouter([
   {path : '#/', component: Home }, // main page
   {path : '#/movie', component: Movie},
-  {path : '#/about', component: About}
+  {path : '#/about', component: About},
+  {path : '.*', component: NotFound} // . 임의의 한 문자. {0,} 0개 이상 (또는 *)
 ])
