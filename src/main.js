3253f0412648c02a6548d330493041317906fe09
woncho <woncho@gmarket.com>
2023-01-19 21:39:36 +0900

    32. Vercel 구성 및 Serverless Functions 테스트

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ import router from './routes'
 const root = document.querySelector('#root')
 root.append(new App().el)
 router()
+
+//즉시 실행 함수
+;(async () => {
+  const res = await fetch('/api/test')
+  const json = await res.json()
+  console.log('/api/test', json);
+})()
